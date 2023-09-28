09540ea767926655cc9ae8439ec37c0ef86b59ea
Euro Rincon Garrido <134429160+rinconeuro@users.noreply.github.com>
2023-09-28 17:20:51 +0000

    segunda entrega

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "a"];
+  let adj = ["great", "big", "beautiful", "creapy", "nasty"];
+  let noun = ["jogger", "racoon", "car", "pizza", "burguer", "thief"];
+  let dom = [".com", ".org", ".es", ".net", ".xyz"];
+
+  function generateDomain() {
+    let selectedPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+    let selectedAdj = adj[Math.floor(Math.random() * adj.length)];
+    let selectedNoun = noun[Math.floor(Math.random() * noun.length)];
+    let selectedDom = dom[Math.floor(Math.random() * dom.length)];
+
+    let domain = selectedPronoun + selectedAdj + selectedNoun + selectedDom;
+
+    return domain;
+  }
+  let generatedDomain = generateDomain();
+  console.log("Generated Domain: " + generatedDomain);
+
+  document;
 };
